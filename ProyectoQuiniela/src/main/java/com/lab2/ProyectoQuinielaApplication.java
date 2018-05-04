7a405d9981b82bfc5df3f189dc8a4bc4b1fa72d0
Marco Saenz <37160343+SullenestDust1@users.noreply.github.com>
2018-05-04 01:16:06 -0400

    Commit 1

--- ProyectoQuiniela/src/main/java/com/lab2/ProyectoQuinielaApplication.java
+++ ProyectoQuiniela/src/main/java/com/lab2/ProyectoQuinielaApplication.java
@@ -2,7 +2,7 @@ package com.lab2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class ProyectoQuinielaApplication {
 
--- ProyectoQuiniela/src/main/java/com/lab2/controlador/Controlador.java
+++ ProyectoQuiniela/src/main/java/com/lab2/controlador/Controlador.java
@@ -2,7 +2,7 @@ package com.lab2.controlador;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+//
 @Controller
 public class Controlador {
 
